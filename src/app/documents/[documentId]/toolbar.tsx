d5d8cc986f624607677c00eb1db04b816e7424a3
drumil ved <drumilhved@gmail.com>
2024-12-18 20:36:46 +0530

    undo, redo, bold, spell check working

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,7 +1,15 @@
 "use client";
-import { LucideIcon, Undo2Icon } from "lucide-react";
+import {
+  BoldIcon,
+  LucideIcon,
+  PrinterIcon,
+  Redo2Icon,
+  SpellCheck,
+  Undo2Icon,
+} from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
+import { Separator } from "@/components/ui/separator";
 
 interface ToolbarButtonsProps {
   onClick?: () => void;
@@ -42,11 +50,48 @@ export default function Toolbar() {
         icon: Undo2Icon,
         onClick: () => editor?.chain().focus().undo().run(),
       },
+      {
+        label: "Redo",
+        icon: Redo2Icon,
+        onClick: () => editor?.chain().focus().redo().run(),
+      },
+      {
+        label: "Print",
+        icon: PrinterIcon,
+        onClick: () => window.print(),
+      },
+      {
+        label: "SpellCheck",
+        icon: SpellCheck,
+        onClick: () => {
+          const current = editor?.view.dom.getAttribute("spellcheck");
+          editor?.view.dom.setAttribute(
+            "spellcheck",
+            current === "false" ? "true" : "false"
+          );
+        },
+      },
+    ],
+    [
+      {
+        label: "Bold",
+        icon: BoldIcon,
+        onClick: () => editor?.chain().focus().toggleBold().run(),
+      },
     ],
   ];
   return (
     <div className=" bg-[#f1f4f9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] gap-x-0.5 flex items-center  overflow-x-auto">
       {sections[0].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/*TODO : FONT FAMILY*/}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/*TODO : HEADING*/}
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {/*TODO : FONT SIZE*/}
+      {sections[1].map((item) => (
         <ToolbarButton key={item.label} {...item} />
       ))}
     </div>
