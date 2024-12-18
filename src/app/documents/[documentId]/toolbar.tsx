9b24a750ed15bd000e6463d5393fe55ae333be5b
drumil ved <drumilhved@gmail.com>
2024-12-18 17:57:46 +0530

    added undo button no functionality

---
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { LucideIcon, Undo2Icon } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface ToolbarButtonsProps {
+  onClick?: () => void;
+  isActive?: boolean;
+  icon?: LucideIcon;
+}
+
+const ToolbarButton = ({
+  onClick,
+  isActive,
+  icon: Icon,
+}: ToolbarButtonsProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80",
+        isActive && "bg-neutral-200/80"
+      )}
+    >
+      {Icon && <Icon className="size-4" />}
+    </button>
+  );
+};
+
+export default function Toolbar() {
+  const sections: {
+    label: string;
+    icon: LucideIcon;
+    onClick: () => void;
+    isActive?: boolean;
+  }[][] = [
+    [
+      {
+        label: "Undo",
+        icon: Undo2Icon,
+        onClick: () => {
+          console.log("Undo Clicked! ");
+        },
+      },
+    ],
+  ];
+  return (
+    <div className=" bg-[#f1f4f9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] gap-x-0.5 flex items-center  overflow-x-auto">
+      {sections[0].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+    </div>
+  );
+}
--- src/app/documents/layout.tsx
+++ src/app/documents/layout.tsx
@@ -2,12 +2,7 @@ interface DocumentsLayoutProps {
   children: React.ReactNode;
 }
 const DocumentsLayout = ({ children }: DocumentsLayoutProps) => {
-  return (
-    <div className="flex gap-y-4 flex-col">
-      <nav className="w-full bg-red-500"> Navbar</nav>
-      {children}
-    </div>
-  );
+  return <div className="flex gap-y-4 flex-col">{children}</div>;
 };
 
 export default DocumentsLayout;
